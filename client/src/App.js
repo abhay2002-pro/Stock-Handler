0a65e74ff2891a5cf2bf2d9d9bde54eca9d95c07
galaxey56 <satyakiran56@gmail.com>
2023-01-15 00:09:59 +0530

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import "./App.css";
 import GraphCard from "./components/GraphCard";
+import LandingPage from "./components/LandingPage";
+import data from './data/data.json'
 
 function App() {
+  console.log(data);
   return (
     <>
-      <GraphCard />
+      <LandingPage data={data}/>
+     {/* <GraphCard /> */}
     </>
   );
 }
